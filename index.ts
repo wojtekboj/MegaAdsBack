a81ef5654504a5c7c898509cf02191925933e220
WB <705235+wojtekboj@users.noreply.github.com>
2023-02-05 09:37:32 +0100

    Added error handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(cors({
 }));
 app.use(json());
 
+// app.get('/', async (req, res) => {
+//     throw new ValidationError('Damn');
+// });
+
+app.use(handleError);
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
 });
