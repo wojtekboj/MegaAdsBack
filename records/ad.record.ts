b40b17324981c201cf89a659d2d39398b84ddc27
WB <705235+wojtekboj@users.noreply.github.com>
2023-02-08 18:15:03 +0100

    Added possibility to add new ads into database.

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -2,6 +2,7 @@ import {AdEntity, NewAdEntity, SimpleAdEntity} from "../types";
 import {ValidationError} from "../utils/errors";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
+import {v4 as uuid} from 'uuid';
 
 type AdRecordsResults = [AdEntity[], FieldPacket[]];
 
@@ -62,6 +63,15 @@ export class AdRecord implements AdEntity {
             return {
                 id, lat ,lon,
             };
-        })
+        });
+    }
+    async insert(): Promise<void> {
+       if (!this.id) {
+           this.id = uuid();
+       } else {
+           throw new Error('Cannot insert something that is already inserted.');
+       }
+
+       await pool.execute("INSERT INTO `ads`(`id`, `name`, `description`, `price`, `url`, `lat`, `lon`) VALUES(:id, :name, :description, :price, :url, :lat, :lon)", this);
     }
 }
